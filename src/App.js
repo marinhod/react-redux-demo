d2d04f6c46057bf2eed49940f1529aad465f9d43
Diego Marinho <marinhop.diego@gmail.com>
2019-01-17 16:37:36 -0200

    refactor: added redux boilerplate to posts component

--- src/App.js
+++ src/App.js
@@ -1,18 +1,22 @@
 import React, { Component } from 'react';
 import './App.css';
+import { Provider } from 'react-redux';
+import store from './store';
 import Form from './components/Form';
 import Posts from './components/Posts';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <h1>React Demo</h1>
-        <hr />
-        <Form />
-        <hr />
-        <Posts />
-      </div>
+      <Provider store={store}>
+        <div className="App">
+          <h1>React Demo</h1>
+          <hr />
+          <Form />
+          <hr />
+          <Posts />
+        </div>
+      </Provider>
     );
   }
 }
--- src/components/Posts.js
+++ src/components/Posts.js
@@ -1,21 +1,15 @@
 import React, { Component } from 'react'
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import { fetchPosts } from '../actions/postActions';
 
 class Posts extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-        posts: []
-    }
-  }
-
   componentWillMount() {
-    fetch('https://jsonplaceholder.typicode.com/posts')
-      .then(response => response.json())
-      .then(json => this.setState({posts: json}))
+    this.props.fetchPosts();
   }
 
   render() {
-    const postItems = this.state.posts.map(post => (
+    const postItems = this.props.posts.map(post => (
       <li key={post.id}>
         <h3>{post.title}</h3>
         <p>{post.body}</p>  
@@ -32,4 +26,13 @@ class Posts extends Component {
   }
 }
 
-export default Posts;
+Posts.PropTypes = {
+  fetchPosts: PropTypes.func.isRequired,
+  posts: PropTypes.array.isRequired
+}
+
+const mapStateToProps = state => ({
+  posts: state.posts.items
+})
+
+export default connect(mapStateToProps, { fetchPosts })(Posts);
